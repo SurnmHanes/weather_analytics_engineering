24812b1a03baa303905006493b84745511cb736d
Neil Hanes <surnamehanes@gmail.com>
2026-08-25 10:35:34 +0100

    Add dbt fact model for weather data

--- models/marts/fact_weather.sql
+++ models/marts/fact_weather.sql
@@ -1,9 +1,18 @@
-FactWeather
-------------
-datetime
-date_key
-location_key
-
-temperature
-precipitation
-wind_speed
+SELECT 
+        {{ dbt_utils.generate_surrogate_key(['stg.observation_datetime', 'stg.location']) }} AS observation_key,
+        d.date_key,
+        l.location_key,
+        stg.observation_datetime,
+        stg.temperature_celsius,
+        stg.precipitation_mm,
+        stg.wind_speed_kmh
+FROM 
+    {{ ref('stg_weather') }} AS stg
+INNER JOIN  
+    {{ ref('dim_date') }} AS d
+ON 
+    CAST(stg.observation_datetime AS DATE) = d.date_day
+INNER JOIN  
+    {{ ref('dim_location') }} AS l
+ON 
+    stg.location = l.location
